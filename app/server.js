cf02748a3cb619016c485a85945047c83fd66605
Victor Nunez <vnunez0914@gmail.com>
2019-10-13 14:12:02 -0500

    added server listener and set up files

---
+++ app/server.js
@@ -0,0 +1,27 @@
+
+// NPM package
+
+var express = require("express");
+
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port for 
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//points the server to "route" files.
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+
+// LISTENER - "starts" our server
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
