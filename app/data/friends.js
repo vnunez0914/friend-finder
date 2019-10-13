0600e9d780474acd58c609ae1a3cf6c67f549bfb
Victor Nunez <vnunez0914@gmail.com>
2019-10-13 14:44:24 -0500

    added images to file friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,49 @@
+var friends = [
+
+      {
+        name: "Pikachu",
+        photo:
+          "app/public/images/pikachu.jpeg",
+        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+      },
+      {
+        name: "Charmander",
+        photo:
+          "app/public/images/charmander.jpeg",
+        scores: [2, 3, 5, 1, 2, 3, 4, 3, 2, 1]
+      },
+      {
+        name: "Squirtle",
+        photo:
+          "app/public/images/squirtle.jpeg",
+        scores: [3, 1, 4, 5, 4, 3, 4, 3, 2, 1]
+      },
+      {
+        name: "Snorlax",
+        photo:
+          "app/public/images/snorlax.jpeg",
+        scores: [1, 2, 4, 3, 5, 1, 2, 5, 4, 1]
+      },
+      {
+        name: "Jigglypuff",
+        photo:
+          "app/public/images/jigglypuff.jpeg",
+        scores: [3, 4, 2, 3, 1, 2, 3, 4, 3, 5]
+      },
+      {
+        name: "Meowth",
+        photo:
+          "app/public/images/meowth.jpeg",
+        scores: [4, 1, 3, 4, 4, 3, 4, 5, 2, 5]
+      },
+      {
+        name: "Onix",
+        photo:
+          "app/public/images/onix.jpeg",
+        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+      },
+    
+    
+    ];
+    
+    module.exports = friends;
